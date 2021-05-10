881f8b8afa319b77831e7f7c906f7b94b0baad15
OiSouHen <oisouhen@gmail.com>
2021-05-10 11:54:31 -0300

    Reconfig start.bat
    Reconfiguration for default database name[creative].

---
+++ server/start.bat
@@ -0,0 +1,2 @@
+@echo off
+..\artifact\FXServer.exe +set onesync on +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +set onesync_forceMigration 1 +set onesync_distanceCullVehicles 1 +exec server.cfg
