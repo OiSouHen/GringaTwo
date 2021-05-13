601bee38c4d2f60e3c94aedacdcb3f827ebb77b0
OiSouHen <oisouhen@gmail.com>
2021-05-13 00:51:57 -0300

    vrp_atm Update
    Removed /atm command, now need Press E to open, and some new atm's are added.

--- server/resources/[Resources]/vrp_atm/client-side/client.lua
+++ server/resources/[Resources]/vrp_atm/client-side/client.lua
@@ -54,32 +54,66 @@ local localidades = {
 	{ -2072.49,-317.37,13.32 },
 	{ 237.84,216.82,106.29 },
 	{ 238.29,215.91,106.29 },
-	{ 237.41,217.74,106.29 },
+	{ 237.41,217.74,106.29 }, 
 	{ 236.76,218.86,106.29 },
-	{ 236.35,219.68,106.29 },
-	{ 146.02,-1035.18,29.35 }
+	{ 236.76,218.86,106.29 },
+	{ 951.32,-986.57,39.67 },
+	{ -200.39,-1309.53,31.3 },
+	{ 1138.28,-468.98,66.73 },
+	{ 1166.96,-456.13,66.81 },
+	{ 285.45,143.44,104.18 },
+	{ 835.09,-1039.29,27.15 },
+	{ 187.44,-899.89,30.72 },
+	{ 33.17,-1348.24,29.5 },
+	{ 2558.49,389.46,108.63 },
+	{ 1153.7,-326.81,69.21 },
+	{ -717.68,-915.65,19.22 },
+	{ -56.9,-1752.09,29.43 },
+	{ 380.83,323.45,103.57 },
+	{ -3241.14,997.61,12.56 },
+	{ -3240.6,1008.59,12.84 },
+	{ -3044.0,594.57,7.74 },
+	{ -3040.75,593.17,7.91 },
+	{ 1735.24,6410.55,35.04 },
+	{ 540.35,2671.09,42.16 },
+	{ 1968.1,3743.57,32.35 },
+	{ 2683.11,3286.58,55.25 },
+	{ 1703.04,4933.58,42.07 },
+	{ -1827.19,784.91,138.31 },
+	{ 1171.55,2702.57,38.18 },
+	{ 1172.51,2702.57,38.18 },
+	{ 264.45,210.03,106.29 },
+	{ 264.78,211.04,106.29 },
+	{ 265.12,211.99,106.29 },
+	{ 265.44,212.97,106.29 },
+	{ 265.77,213.9,106.29 },
+	{ 288.76,-1282.33,29.65 },
+	{ 289.02,-1256.83,29.45 },
+	{ 146.02,-1035.18,29.35 },
+	{ 1144.16,-1531.46,35.39 },
+	{ 1701.25,6426.62,32.77 }
 }
 -----------------------------------------------------------------------------------------------------------------------------------------
--- ATM
+-- ATMCOMMAND
 -----------------------------------------------------------------------------------------------------------------------------------------
-RegisterCommand("atm",function()
-	local ped = PlayerPedId()
-	local coords = GetEntityCoords(ped)
-
-	for k,v in pairs(atmObjects) do
-		if DoesObjectOfTypeExistAtCoords(coords,0.6,GetHashKey(v),true) then
-			local saldo = vSERVER.getSaldo()
-			SetNuiFocus(true,true)
-			SendNUIMessage({ action = "show", saldo = tostring(saldo) })
-			vRP._playAnim(false,{"amb@prop_human_atm@male@base","base"},true)
-		end
-	end
-end)
+-- RegisterCommand("atm",function()
+-- 	local ped = PlayerPedId()
+-- 	local coords = GetEntityCoords(ped)
+	
+-- 	for k,v in pairs(atmObjects) do
+-- 		if DoesObjectOfTypeExistAtCoords(coords,0.6,GetHashKey(v),true) then
+-- 			local saldo = vSERVER.getSaldo()
+-- 			SetNuiFocus(true,true)
+-- 			SendNUIMessage({ action = "show", saldo = tostring(saldo) })
+-- 			vRP._playAnim(false,{"amb@prop_human_atm@male@base","base"},true)
+-- 		end
+-- 	end
+-- end)
 -----------------------------------------------------------------------------------------------------------------------------------------
--- OPENSHOP
+-- OPENATM
 -----------------------------------------------------------------------------------------------------------------------------------------
 Citizen.CreateThread(function()
-			SetNuiFocus(false,false)
+	SetNuiFocus(false,false)
 	while true do
 		local sleep = 500
 		local ped = PlayerPedId()
@@ -89,11 +123,11 @@ Citizen.CreateThread(function()
 				local distance = #(coords - vector3(v[1],v[2],v[3]))
 				if distance <= 1 then
 					sleep = 4
-					DrawText3D(v[1],v[2],v[3],"~g~E~w~   ABRIR")
-					if IsControlJustPressed(1,38) --[[ and vSERVER.requestWanted()]] then				
+					if IsControlJustPressed(1,38) then -- vSERVER.requestWanted()
 						SetNuiFocus(true,true)
-		local saldo = vSERVER.getSaldo()			    
-				SendNUIMessage({ action = "show", saldo = tostring(saldo) })
+						local saldo = vSERVER.getSaldo()			    
+						SendNUIMessage({ action = "show", saldo = tostring(saldo) })
+						vRP._playAnim(false,{"amb@prop_human_atm@male@base","base"},true)
 					end
 				end
 			end
@@ -134,11 +168,11 @@ function DrawText3D(x,y,z,text)
 	local onScreen,_x,_y = World3dToScreen2d(x,y,z)
 	SetTextFont(4)
 	SetTextScale(0.35,0.35)
-	SetTextColour(255,255,255,100)
+	SetTextColour(176,180,193,150)
 	SetTextEntry("STRING")
 	SetTextCentre(1)
 	AddTextComponentString(text)
 	DrawText(_x,_y)
-	local factor = (string.len(text)) / 400
-	DrawRect(_x,_y+0.0125,0.01+factor,0.03,0,0,0,100)
+	local factor = (string.len(text))/350
+	DrawRect(_x,_y+0.0125,0.01+factor,0.04,50,55,67,150)
 end
