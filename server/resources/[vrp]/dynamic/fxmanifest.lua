53e94a317edafcfd21598e9536f38c55bc56c122
OiSouHen <oisouhen@gmail.com>
2021-05-30 14:15:03 -0300

    Update fxmanifest.lua

--- server/resources/[vrp]/dynamic/fxmanifest.lua
+++ server/resources/[vrp]/dynamic/fxmanifest.lua
@@ -4,16 +4,12 @@ game "gta5"
 ui_page "web-side/index.html"
 
 client_scripts {
-	"@vrp/server-side/garages.lua",
-	"@vrp/server-side/inventory.lua",
 	"@PolyZone/client.lua",
 	"@vrp/lib/utils.lua",
 	"client-side/*"
 }
 
 server_scripts {
-	"@vrp/server-side/garages.lua",
-	"@vrp/server-side/inventory.lua",
 	"@vrp/lib/utils.lua",
 	"server-side/*"
 }
