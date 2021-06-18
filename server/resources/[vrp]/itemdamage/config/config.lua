85c5ba9cbf6141edfc368d2853cd2b814699240e
OiSouHen <oisouhen@gmail.com>
2021-06-18 16:01:07 -0300

    Update config.lua

--- server/resources/[vrp]/itemdamage/config/config.lua
+++ server/resources/[vrp]/itemdamage/config/config.lua
@@ -2,6 +2,7 @@ config = {}
 
 config.itemList = {
 	{ ['item'] = 'cellphone', ['damageItem'] = 'cellphonedamaged' },
+	{ ['item'] = 'nbcellphone', ['damageItem'] = 'cellphonedamaged' },
 	{ ['item'] = 'radio', ['damageItem'] = 'radiodamaged' },
 	{ ['item'] = 'dollars', ['damageItem'] = '' },
 	{ ['item'] = 'dollars2', ['damageItem'] = '' },
