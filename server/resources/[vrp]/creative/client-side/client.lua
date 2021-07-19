2783cda0853da9c3d372f97aa7290048768ff4a4
OiSouHen <oisouhen@gmail.com>
2021-07-19 00:15:45 -0300

    Update client.lua

--- server/resources/[vrp]/creative/client-side/client.lua
+++ server/resources/[vrp]/creative/client-side/client.lua
@@ -1031,6 +1031,28 @@ function HandleZoom(cam)
 	SetCamFov(cam,current_fov + (fov - current_fov) * 0.05)
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
+-- BUNNYHOP
+-----------------------------------------------------------------------------------------------------------------------------------------
+local bunnyHope = GetGameTimer()
+Citizen.CreateThread(function()
+	while true do
+		local timeDistance = 999
+		local ped = PlayerPedId()
+		if not IsPedInAnyVehicle(ped) then
+			if GetGameTimer() <= bunnyHope then
+				timeDistance = 1
+				DisableControlAction(1,22,true)
+			else
+				if IsPedJumping(ped) then
+					bunnyHope = GetGameTimer() + 5000
+				end
+			end
+		end
+
+		Citizen.Wait(timeDistance)
+	end
+end)
+-----------------------------------------------------------------------------------------------------------------------------------------
 -- Rich Presence
 -----------------------------------------------------------------------------------------------------------------------------------------
 Citizen.CreateThread(function()
