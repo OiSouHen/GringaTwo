b91e59a92b62d581f554f93cbca6d219fef9a04e
Lucas Hen <oisouhen@gmail.com>
2021-07-30 22:34:05 -0300

    Fixed airforce job

--- server/resources/[jobs]/airforce/server-side/server.lua
+++ server/resources/[jobs]/airforce/server-side/server.lua
@@ -18,23 +18,17 @@ local paymentMax = 250
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- paymentService
 -----------------------------------------------------------------------------------------------------------------------------------------
-function cRP.paymentService()
+function cRP.paymentService(status)
 	local source = source
 	local user_id = vRP.getUserId(source)
 	if user_id then
-		if vRP.computeInvWeight(user_id) + 1 > vRP.getBackpack(user_id) then
-			TriggerClientEvent("Notify",source,"vermelho","Espaço insuficiente.",5000)
-			Wait(1)
-		else
-			
-			vRP.upgradeStress(user_id,1)
-			local value = math.random(paymentMin,paymentMax) * amount[source]
+		local value = math.random(175,255)
+
+		if not status then
 			vRP.giveInventoryItem(user_id,"dollars",parseInt(value),true)
-			TriggerClientEvent("sound:source",source,"coin",0.5)
-			
-			amount[source] = nil
-			return true
+		else
+			vRP.giveInventoryItem(user_id,"dollars",parseInt(value),true)
 		end
-		return false
+		TriggerClientEvent("sound:source",source,"coin",0.5)
 	end
 end
