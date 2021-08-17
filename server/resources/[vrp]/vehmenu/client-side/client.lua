a216be602ca57db4f7368a4eb99b3f36d21ba2ed
Lucas Hen <oisouhen@gmail.com>
2021-08-17 13:15:52 -0300

    Vehmenu update

--- server/resources/[vrp]/vehmenu/client-side/client.lua
+++ server/resources/[vrp]/vehmenu/client-side/client.lua
@@ -48,38 +48,34 @@ end)
 -- MENUACTIVE
 -----------------------------------------------------------------------------------------------------------------------------------------
 RegisterNUICallback("menuActive",function(data)
-	if data["active"] == "chest" then
-		TriggerServerEvent("trunkchest:openTrunk")
+	local ped = PlayerPedId()
+	if GetVehiclePedIsTryingToEnter(ped) <= 0 then
+		if data["active"] == "chest" then
+			TriggerServerEvent("trunkchest:openTrunk")
 
-		SendNUIMessage({ show = false })
-		SetCursorLocation(0.5,0.5)
-		SetNuiFocus(false,false)
-		vehMenu = false
-	elseif data["active"] == "door1" then
-		TriggerServerEvent("vehmenu:doors","1")
-	elseif data["active"] == "door2" then
-		TriggerServerEvent("vehmenu:doors","2")
-	elseif data["active"] == "door3" then
-		TriggerServerEvent("vehmenu:doors","3")
-	elseif data["active"] == "door4" then
-		TriggerServerEvent("vehmenu:doors","4")
-	elseif data["active"] == "trunk" then
-		TriggerServerEvent("vehmenu:doors","5")
-	elseif data["active"] == "hood" then
-		TriggerServerEvent("vehmenu:doors","6")
-	elseif data["active"] == "trunkin" then
-		TriggerEvent("player:EnterTrunk")
+			SendNUIMessage({ show = false })
+			SetCursorLocation(0.5,0.5)
+			SetNuiFocus(false,false)
+			vehMenu = false
+		elseif data["active"] == "door1" then
+			TriggerServerEvent("vehmenu:doors","1")
+		elseif data["active"] == "door2" then
+			TriggerServerEvent("vehmenu:doors","2")
+		elseif data["active"] == "door3" then
+			TriggerServerEvent("vehmenu:doors","3")
+		elseif data["active"] == "door4" then
+			TriggerServerEvent("vehmenu:doors","4")
+		elseif data["active"] == "trunk" then
+			TriggerServerEvent("vehmenu:doors","5")
+		elseif data["active"] == "hood" then
+			TriggerServerEvent("vehmenu:doors","6")
+		elseif data["active"] == "vtuning" then
+			TriggerEvent("engine:vehTuning")
 
-		SendNUIMessage({ show = false })
-		SetCursorLocation(0.5,0.5)
-		SetNuiFocus(false,false)
-		vehMenu = false
-	elseif data["active"] == "vtuning" then
-		TriggerEvent("engine:vehTuning")
-
-		SendNUIMessage({ show = false })
-		SetCursorLocation(0.5,0.5)
-		SetNuiFocus(false,false)
-		vehMenu = false
+			SendNUIMessage({ show = false })
+			SetCursorLocation(0.5,0.5)
+			SetNuiFocus(false,false)
+			vehMenu = false
+		end
 	end
 end)
